b26a256f14614469d333faba6f5c006bd8d19fca
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-08-02 19:21:18 +0530

    corrected code

--- Assignment3/assignment 3 optional.sql
+++ Assignment3/assignment 3 optional.sql
@@ -14,15 +14,15 @@ insert all
 select *from dual;
 
 insert all
-	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values(P00001, '1.44 floppies', 5, 'Piece', 100, 20, 525, 500)
-	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values(P03453, 'Monitors', 6, 'Piece', 10, 3, 12000, 11200)
-	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values(P06734, 'Mouse', 5, 'piece', 20, 5, 1050, 500)
-	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values(P07865, '1.22 floppies', 5, 'Piece', 100, 20, 525, 500)
-	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values(P07868, 'Keyboards', 2, 'Piece', 10, 3, 3150, 3050)
-	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values(P07885, 'CD Drive', 2.5, 'Piece', 10, 3, 5250, 5100)
-	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values(P07965, '540 HDD', 4, 'Piece', 10, 3, 8400, 8000)
-	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values(P07975, '1.44 Drive', 5, 'Piece', 10, 3, 1050, 1000)
-	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values(P08865, '1.22 Drive', 5, 'Piece', 2, 3, 1000, 920)
+	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values('P00001', '1.44 floppies', 5, 'Piece', 100, 20, 525, 500)
+	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values('P03453', 'Monitors', 6, 'Piece', 10, 3, 12000, 11200)
+	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values('P06734', 'Mouse', 5, 'piece', 20, 5, 1050, 500)
+	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values('P07865', '1.22 floppies', 5, 'Piece', 100, 20, 525, 500)
+	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values('P07868', 'Keyboards', 2, 'Piece', 10, 3, 3150, 3050)
+	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values('P07885', 'CD Drive', 2.5, 'Piece', 10, 3, 5250, 5100)
+	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values('P07965', '540 HDD', 4, 'Piece', 10, 3, 8400, 8000)
+	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values('P07975', '1.44 Drive', 5, 'Piece', 10, 3, 1050, 1000)
+	into PRODUCT_MASTER(productno, description, profit_percent, unit_measure, qty_on_hand, reorder_lvlnumber, sell_price, cost_price_number) values('P08865', '1.22 Drive', 5, 'Piece', 2, 3, 1000, 920)
 select *from dual;
 
 
