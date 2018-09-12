fc64e97515eeed365d1f3d49ee27e3777df18e85
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-09-12 12:49:31 +0530

    ass 5

---
+++ Assignment5/assignment5.sql
@@ -0,0 +1,10 @@
+/*
+Name: Nikhil Ranjan Nayak
+Regd No: 1641012040
+Desc: Assignment 5
+*/
+
+i.
+1) select name, phone_no, cust_no from CUSTOMER natural join DEPOSITOR where DEPOSITOR.account_no = 'A0004';
+
+2) select loan_amount, branch_code from 
