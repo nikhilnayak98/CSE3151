36761f83102be56fb51c466420bd1a45f1b391de
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-09-19 12:51:24 +0530

    ass 5

--- Assignment5/assignment5.sql
+++ Assignment5/assignment5.sql
@@ -7,4 +7,22 @@ Desc: Assignment 5
 i.
 1) select name, phone_no, cust_no from CUSTOMER natural join DEPOSITOR where DEPOSITOR.account_no = 'A0004';
 
-2) select loan_amount, branch_code from 
+   select name, phone_no, cust_no from CUSTOMER where cust_no in (select cust_no from DEPOSITOR where account_no = 'A0004');
+
+2) select amount, branch_code from LOAN natural join CUSTOMER where CUSTOMER.name = 'YASH SARAF';
+
+   select amount, branch_code from LOAN where cust_no in (select cust_no from CUSTOMER where name = 'YASH SARAF');
+
+3) select CUSTOMER.name from CUSTOMER left join LOAN on CUSTOMER.cust_no = LOAN.cust_no where LOAN.cust_no is NULL;
+
+  select name from CUSTOMER where cust_no not in (select cust_no from LOAN);
+
+4) select account_no, balance from ACCOUNT where account_no in (select account_no from DEPOSITOR where cust_no = 'C0010');
+
+5) select city from CUSTOMER where cust_no in (select cust_no from CUSTOMER where name = 'ASLESHA TIWARI');
+
+6) select type, branch_code from ACCOUNT where account_no in (select account_no from DEPOSITOR where cust_no = 'C0002');
+
+7) select A.account_no, A.balance from ACCOUNT A where A.type = 'SB';
+
+8) select *from INSTALLMENT where loan_no in (select loan_no from LOAN where cust_no = 'C0001');
